64db4c884e57fc533a12299e395f081bb4f2e6bd
Or Jerbi <96544670+Or-Jerbi@users.noreply.github.com>
2025-07-13 15:33:07 +0300

    Add files via upload

---
+++ client/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { getAllElevatorCalls } from "./services/elevatorCallService";
+import Register from "./pages/Register/registerPage";
+import BuildingManagement from "./pages/BuildingManagement/buildingManagementPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/Login/LoginPage";
+import BuildingPage from "./pages/BuildingPage/BuildingPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<BuildingManagement />} />
+          <Route path="/building/:id" element={<BuildingPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
